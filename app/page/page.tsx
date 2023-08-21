3719ed32edbaa21664e245bf99154361b1211966
Lukas Prochazka <prochazka.lu@gmail.com>
2023-08-21 13:46:01 +0200

    Add Image component

--- app/page/page.tsx
+++ app/page/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <main>
       <h1>Page</h1>
-      {/* <Image src="/next.svg" alt="" width="300" height="300" /> */}
+      <Image src="/next.svg" alt="" width="300" height="300" />
     </main>
   );
 }
