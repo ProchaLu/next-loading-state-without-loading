47061f38702e13d771ab94c625d3ff6ba134957e
Lukas Prochazka <prochazka.lu@gmail.com>
2023-09-18 17:24:06 +0200

    Change fetch

--- app/fetch/GeneratePokemon.tsx
+++ app/fetch/GeneratePokemon.tsx
@@ -1,22 +1,13 @@
-'use client';
-
-import { useEffect, useState } from 'react';
-
-export default function GeneratePokemon() {
-  const [pokemon, setPokemon] = useState<string | undefined>();
-
+export default async function GeneratePokemon() {
   async function getPokemon() {
     const response = await fetch(
       `https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random() * 151)}`,
     );
     const data = await response.json();
-
-    setPokemon(data.name);
+    return data.name;
   }
 
-  useEffect(() => {
-    getPokemon().catch(() => {});
-  }, []);
+  const pokemon = await getPokemon();
 
   return <main>This in a {pokemon} page</main>;
 }
