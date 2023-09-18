35d8e959a0ca801020286dbf2d852b8aedd35ef3
Lukas Prochazka <prochazka.lu@gmail.com>
2023-09-18 17:32:02 +0200

    Update pokemon page

--- app/fetch/page.tsx
+++ app/fetch/page.tsx
@@ -3,7 +3,9 @@ import GeneratePokemon from './GeneratePokemon';
 export default function PokemonPage() {
   return (
     <main>
+      <h1>Pokemon Page</h1>
       <GeneratePokemon />
+      <p>lorem....</p>
     </main>
   );
 }
