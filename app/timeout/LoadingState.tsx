2a5ebd6fd192bf90e5480f564e100a58d60a27c4
Lukas Prochazka <prochazka.lu@gmail.com>
2023-09-20 11:29:02 +0200

    Add LoadingState page

---
+++ app/timeout/LoadingState.tsx
@@ -0,0 +1,29 @@
+'use client';
+import { useEffect, useState } from 'react';
+
+function delay() {
+  return new Promise<void>((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, 2000);
+  });
+}
+
+export default function LoadingState() {
+  const [data, setData] = useState<string[] | undefined>();
+  useEffect(() => {
+    delay()
+      .then(() => {
+        setData(['one', 'two', 'three']);
+      })
+      .catch(() => {});
+  }, []);
+  return (
+    <div>
+      <h1>Loading Page</h1>
+      {data?.map((item) => {
+        return <div key={`item-${item}`}>{item}</div>;
+      })}
+    </div>
+  );
+}
--- app/timeout/page.tsx
+++ app/timeout/page.tsx
@@ -1,29 +1,5 @@
-'use client';
-import { useEffect, useState } from 'react';
-
-function delay() {
-  return new Promise<void>((resolve) => {
-    setTimeout(() => {
-      resolve();
-    }, 4000);
-  });
-}
+import LoadingState from './LoadingState';
 
 export default function LoadingPage() {
-  const [data, setData] = useState<string[] | undefined>();
-  useEffect(() => {
-    delay()
-      .then(() => {
-        setData(['one', 'two', 'three']);
-      })
-      .catch(() => {});
-  }, []);
-  return (
-    <div>
-      <h1>Loading Page</h1>
-      {data?.map((item) => {
-        return <div key={item}>{item}</div>;
-      })}
-    </div>
-  );
+  return <LoadingState />;
 }
